636a56795cb3db1843b0de53182fe965d8bb6b82
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-10-06 09:05:17 +0300

    fix: Encapsulate LiveData objects in GameViewModel

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,6 +17,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -27,20 +28,30 @@ import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+    get() = _word
+
 
     // The current score
-    var score = MutableLiveData<Int>()
-    var eventGameFinished = MutableLiveData<Boolean>()
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+    get() = _score
+
+    private val _eventGameFinished = MutableLiveData<Boolean>()
+    val eventGameFinished: LiveData<Boolean>
+    get() = _eventGameFinished
+
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
 
     init {
-        eventGameFinished.value=false
+        _eventGameFinished.value=false
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
         Log.i("GameViewModel", "GameViewModel created!")
     }
 
@@ -81,9 +92,9 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         //Select and remove a word from the list
         if (wordList.isEmpty()) {
-            eventGameFinished.value=true
+            _eventGameFinished.value=true
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
 
     }
@@ -91,12 +102,12 @@ class GameViewModel : ViewModel() {
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score.value= (score.value)?.minus(1)
+        _score.value= (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value= (score.value)?.plus(1)
+        _score.value= (score.value)?.plus(1)
         nextWord()
     }
 
@@ -107,6 +118,6 @@ class GameViewModel : ViewModel() {
 
     //eventGameFinished should occur once, fragment recreation shouldn't trigger it again. set its value to false
     fun gameFinishedComplete(){
-        eventGameFinished.value = false
+        _eventGameFinished.value = false
     }
 }
